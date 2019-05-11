7159a472d808c4c2037354d87fee46d8bb09e579
Nedov <49785108+Nedov@users.noreply.github.com>
2019-05-11 17:03:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,46 +102,46 @@ const code = args.join(" ")
 });
 
 
-// const size = config.colors;
-// const rainbow = new Array(size);
+const size = config.colors;
+const rainbow = new Array(size);
 
-// for (var i=0; i<size; i++) {
-// var red = sin_to_hex(i, 0 * Math.PI * 2/3); // 0 deg
-// var blue = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
-// var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+for (var i=0; i<size; i++) {
+var red = sin_to_hex(i, 0 * Math.PI * 2/3); // 0 deg
+var blue = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
 
-// rainbow[i] = '#'+ red + green + blue;
-// }
+rainbow[i] = '#'+ red + green + blue;
+}
 
-// function sin_to_hex(i, phase) {
-// var sin = Math.sin(Math.PI / size * 2 * i + phase);
-// var int = Math.floor(sin * 127) + 128;
-// var hex = int.toString(16);
+function sin_to_hex(i, phase) {
+var sin = Math.sin(Math.PI / size * 2 * i + phase);
+var int = Math.floor(sin * 127) + 128;
+var hex = int.toString(16);
 
-// return hex.length === 1 ? '0'+hex : hex;
-// }
+return hex.length === 1 ? '0'+hex : hex;
+}
 
-// let place = 0;
-// const serverss = ["528264174338310206"]
+let place = 0;
+const serverss = ["528264174338310206"]
 
-// function changeColor() {
-// for (let index = 0; index < serverss.length; ++index) {
+function changeColor() {
+for (let index = 0; index < serverss.length; ++index) {
 
-// bot.guilds.get(serverss[index]).roles.find(r => r.name === "Г").setColor(rainbow[place])
-//     .catch(console.error);
+bot.guilds.get(serverss[index]).roles.find(r => r.name === "шамана").setColor(rainbow[place])
+    .catch(console.error);
 
-// if(place == (size - 1)){
-// place = 0;
-// }else{
-// place++;
-// }
-// }
-// }
+if(place == (size - 1)){
+place = 0;
+}else{
+place++;
+}
+}
+}
 
-// bot.on('ready', () => {
-// if(config.speed < 10){ process.exit(1);}
-// setInterval(changeColor, config.speed);
-// });
+bot.on('ready', () => {
+if(config.speed < 10){ process.exit(1);}
+setInterval(changeColor, config.speed);
+});
 
 
 
