87774103a4edeb6bb60a7179a70cd50c243f7b02
Nedov <49785108+Nedov@users.noreply.github.com>
2019-06-10 16:52:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,17 +10,61 @@ let ms = require('ms');
 let config = require('./config.json');
 
 
-function randColor() {
-    var r = Math.floor(Math.random() * (256)),
-        g = Math.floor(Math.random() * (256)),
-        b = Math.floor(Math.random() * (256));
-    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+// function randColor() {
+//     var r = Math.floor(Math.random() * (256)),
+//         g = Math.floor(Math.random() * (256)),
+//         b = Math.floor(Math.random() * (256));
+//     return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+// }
+// setInterval(() => {
+// bot.guilds.get("552169067260149772").roles.find(r => r.name === "Лоликонщик").setColor(randColor())
+// }, 1500)
+
+
+
+
+
+
+const size = config.colors;
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+var red = sin_to_hex(i, 0 * Math.PI * 2/3); // 0 deg
+var blue = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+
+rainbow[i] = '#'+ red + green + blue;
 }
-setInterval(() => {
-bot.guilds.get("552169067260149772").roles.find(r => r.name === "Лоликонщик").setColor(randColor())
-}, 1500)
 
+function sin_to_hex(i, phase) {
+var sin = Math.sin(Math.PI / size * 2 * i + phase);
+var int = Math.floor(sin * 127) + 128;
+var hex = int.toString(16);
 
+return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+const serverss = ["552169067260149772"]
+
+function changeColor() {
+for (let index = 0; index < serverss.length; ++index) {
+
+bot.guilds.get(serverss[index]).roles.find(r => r.name === "Лоликонщик").setColor(rainbow[place])
+    .catch(console.error);
+
+if(place == (size - 1)){
+place = 0;
+}else{
+place++;
+}
+}
+}
+
+bot.on('ready', () => {
+if(config.speed < 10){ process.exit(1);}
+setInterval(changeColor, config.speed);
+});
 
 
 
