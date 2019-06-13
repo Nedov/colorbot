30a16025509d81a6a788ed4b68218be573ba68ab
Nedov <49785108+Nedov@users.noreply.github.com>
2019-06-14 00:32:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,19 +12,19 @@ let config = require('./config.json');
 bot.on('ready', () => {
       console.log(`Запустился бот ${bot.user.username}!`);
 
-    function randColor() {
-        var r = Math.floor(Math.random() * (256)),
-            g = Math.floor(Math.random() * (256)),
-            b = Math.floor(Math.random() * (256));
-        return '#' + r.toString(16) + g.toString(16) + b.toString(16);
-    }
-    setInterval(() => {
-    bot.guilds.get("481763555494985749").roles.find(r => r.name === "Цвет").setColor(randColor())
-    }, 1500)
+//     function randColor() {
+//         var r = Math.floor(Math.random() * (256)),
+//             g = Math.floor(Math.random() * (256)),
+//             b = Math.floor(Math.random() * (256));
+//         return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+//     }
+//     setInterval(() => {
+//     bot.guilds.get("481763555494985749").roles.find(r => r.name === "Цвет").setColor(randColor())
+//     }, 1500)
       
-    setInterval(() => {
-      bot.channels.get('576781347453141002').send('5')
-    }, 60000)  
+//     setInterval(() => {
+//       bot.channels.get('576781347453141002').send('5')
+//     }, 60000)  
      
     
     
@@ -35,46 +35,46 @@ bot.on('ready', () => {
 
 
 
-// const size = config.colors;
-// const rainbow = new Array(size);
+const size = config.colors;
+const rainbow = new Array(size);
 
-// for (var i=0; i<size; i++) {
-// var red = sin_to_hex(i, 0 * Math.PI * 2/3); // 0 deg
-// var blue = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
-// var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+for (var i=0; i<size; i++) {
+var red = sin_to_hex(i, 0 * Math.PI * 2/3); // 0 deg
+var blue = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
 
-// rainbow[i] = '#'+ red + green + blue;
-// }
+rainbow[i] = '#'+ red + green + blue;
+}
 
-// function sin_to_hex(i, phase) {
-// var sin = Math.sin(Math.PI / size * 2 * i + phase);
-// var int = Math.floor(sin * 127) + 128;
-// var hex = int.toString(16);
+function sin_to_hex(i, phase) {
+var sin = Math.sin(Math.PI / size * 2 * i + phase);
+var int = Math.floor(sin * 127) + 128;
+var hex = int.toString(16);
 
-// return hex.length === 1 ? '0'+hex : hex;
-// }
+return hex.length === 1 ? '0'+hex : hex;
+}
 
-// let place = 0;
-// const serverss = ["552169067260149772"]
+let place = 0;
+const serverss = ["481763555494985749"]
 
-// function changeColor() {
-// for (let index = 0; index < serverss.length; ++index) {
+function changeColor() {
+for (let index = 0; index < serverss.length; ++index) {
 
-// bot.guilds.get(serverss[index]).roles.find(r => r.name === "Лоликонщик").setColor(rainbow[place])
-//     .catch(console.error);
+bot.guilds.get(serverss[index]).roles.find(r => r.name === "Цвет").setColor(rainbow[place])
+    .catch(console.error);
 
-// if(place == (size - 1)){
-// place = 0;
-// }else{
-// place++;
-// }
-// }
-// }
+if(place == (size - 1)){
+place = 0;
+}else{
+place++;
+}
+}
+}
 
-// bot.on('ready', () => {
-// if(config.speed < 10){ process.exit(1);}
-// setInterval(changeColor, config.speed);
-// });
+bot.on('ready', () => {
+if(config.speed < 10){ process.exit(1);}
+setInterval(changeColor, config.speed);
+});
 
 
 
