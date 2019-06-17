594d14a031038a442e1c97369ea692675a6a95b4
Nedov <49785108+Nedov@users.noreply.github.com>
2019-06-17 22:58:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,35 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+bot.mutes = require("./mutes.json");
+let moment = require('moment');
+let ms = require('ms');
 let config = require('./config.json');
-const Discord = require("discord.js");
-const robot = new Discord.Client();
-const prefix = "="
-const moment = require('moment');
 
-robot.on('ready', () => {
-    console.log(`Запустился бот ${robot.user.username}!`);
+bot.on('ready', () => {
+      console.log(`Запустился бот ${bot.user.username}!`);
 
-//   function randColor() {
-//       var r = Math.floor(Math.random() * (256)),
-//           g = Math.floor(Math.random() * (256)),
-//           b = Math.floor(Math.random() * (256));
-//       return '#' + r.toString(16) + g.toString(16) + b.toString(16);
-//   }
-//   setInterval(() => {
-//   robot.guilds.get("510527824894099457").roles.find(r => r.name === "color").setColor(randColor())
-//   }, 1500)
-robot.generateInvite(["ADMINISTRATOR"]).then(link =>{
-    console.log(link);
-  })    
-robot.user.setPresence({
-      game:{
-        name:"Собственность Sequel",
-        type:"STREAMING",
-        url:"https://twitch.tv/sequel"
-      }
-    });    
-   
-  
-  
+//     function randColor() {
+//         var r = Math.floor(Math.random() * (256)),
+//             g = Math.floor(Math.random() * (256)),
+//             b = Math.floor(Math.random() * (256));
+//         return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+//     }
+//     setInterval(() => {
+//     bot.guilds.get("510527824894099457").roles.find(r => r.name === "color").setColor(randColor())
+//     }, 1500)
+      
+
+     
     
     
-  });
+      
+      
+    });
+
+
+
+
 const size = config.colors;
 const rainbow = new Array(size);
 
@@ -57,7 +55,7 @@ const serverss = ["510527824894099457"]
 function changeColor() {
 for (let index = 0; index < serverss.length; ++index) {
 
-robot.guilds.get(serverss[index]).roles.find(r => r.name === "color").setColor(rainbow[place])
+bot.guilds.get(serverss[index]).roles.find(r => r.name === "color").setColor(rainbow[place])
     .catch(console.error);
 
 if(place == (size - 1)){
@@ -68,9 +66,9 @@ place++;
 }
 }
 
-robot.on('ready', () => {
+bot.on('ready', () => {
 if(config.speed < 10){ process.exit(1);}
 setInterval(changeColor, config.speed);
 });
 
-robot.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
